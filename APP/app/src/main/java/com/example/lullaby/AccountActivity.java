567b2986b2e9c2c9f3f359f2dbb598c3758cb37c
11liztaion <11lization@naver.com>
2021-11-25 15:59:30 +0900

    account page & button

--- APP/app/src/main/java/com/example/lullaby/AccountActivity.java
+++ APP/app/src/main/java/com/example/lullaby/AccountActivity.java
@@ -2,7 +2,14 @@ package com.example.lullaby;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class AccountActivity extends AppCompatActivity {
 
@@ -10,5 +17,75 @@ public class AccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
+        ToggleButton eunseoButton =
+                (ToggleButton) this.findViewById(R.id.eunseoButton);
+        eunseoButton.setChecked(true);
+        ToggleButton minjiButton =
+                (ToggleButton) this.findViewById(R.id.minjiButton);
+        ToggleButton myeongkyunButton =
+                (ToggleButton) this.findViewById(R.id.myeongkyunButton);
+        ToggleButton haeunButton =
+                (ToggleButton) this.findViewById(R.id.haeunButton);
+
+        eunseoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            if (eunseoButton.isChecked()) {
+                eunseoButton.setBackgroundDrawable(getResources().getDrawable(R.drawable.account_eunseo_s));
+                minjiButton.setBackgroundDrawable(getResources().getDrawable(R.drawable.account_minji));
+                minjiButton.setChecked(false);
+                myeongkyunButton.setBackgroundDrawable(getResources().getDrawable(R.drawable.account_myeong));
+                myeongkyunButton.setChecked(false);
+                haeunButton.setBackgroundDrawable(getResources().getDrawable(R.drawable.account_haeun));
+                haeunButton.setChecked(false);
+                } else {
+                }
+            }
+        });
+        minjiButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (minjiButton.isChecked()) {
+                    eunseoButton.setBackgroundDrawable(getResources().getDrawable(R.drawable.account_eunseo));
+                    eunseoButton.setChecked(false);
+                    minjiButton.setBackgroundDrawable(getResources().getDrawable(R.drawable.account_minji_s));
+                    myeongkyunButton.setBackgroundDrawable(getResources().getDrawable(R.drawable.account_myeong));
+                    myeongkyunButton.setChecked(false);
+                    haeunButton.setBackgroundDrawable(getResources().getDrawable(R.drawable.account_haeun));
+                    haeunButton.setChecked(false);
+                } else {
+                }
+            }
+        });
+        myeongkyunButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (myeongkyunButton.isChecked()) {
+                    eunseoButton.setBackgroundDrawable(getResources().getDrawable(R.drawable.account_eunseo));
+                    eunseoButton.setChecked(false);
+                    minjiButton.setBackgroundDrawable(getResources().getDrawable(R.drawable.account_minji));
+                    minjiButton.setChecked(false);
+                    myeongkyunButton.setBackgroundDrawable(getResources().getDrawable(R.drawable.account_myeong));
+                    haeunButton.setBackgroundDrawable(getResources().getDrawable(R.drawable.account_haeun));
+                    haeunButton.setChecked(false);
+                } else {
+                }
+            }
+        });
+        haeunButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (haeunButton.isChecked()) {
+                    eunseoButton.setBackgroundDrawable(getResources().getDrawable(R.drawable.account_eunseo));
+                    eunseoButton.setChecked(false);
+                    minjiButton.setBackgroundDrawable(getResources().getDrawable(R.drawable.account_minji));
+                    minjiButton.setChecked(false);
+                    myeongkyunButton.setBackgroundDrawable(getResources().getDrawable(R.drawable.account_myeong));
+                    myeongkyunButton.setChecked(false);
+                    haeunButton.setBackgroundDrawable(getResources().getDrawable(R.drawable.account_haeun));
+                } else {
+                }
+            }
+        });
     }
 }
