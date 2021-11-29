3d35844ff53b20019df9e7c67bd4e39f76d4c6e3
11liztaion <11lization@naver.com>
2021-11-29 15:55:47 +0900

    update account data

--- APP/app/src/main/java/com/example/lullaby/AccountData.java
+++ APP/app/src/main/java/com/example/lullaby/AccountData.java
@@ -3,9 +3,17 @@ package com.example.lullaby;
 public class AccountData {
 
     private static AccountData accountData = null;
+    private int idx;
+    private String userId;
     private String name;
     private String gender;
     private int age;
+    private String category1;
+    private String category2;
+
+    public int getIdx() { return idx;}
+
+    public String getUserId() { return userId; }
 
     public String getName(){
         return name;
@@ -15,20 +23,34 @@ public class AccountData {
         return gender;
     }
 
-    public int getBirth(){
+    public int getAge(){
         return age;
     }
+
+    public String getCategory1() { return category1; }
+
+    public String getCategory2() { return category2; }
 
     public void setName(String name){
         this.name = name;
     }
 
+    public void setIdx(int idx) { this.idx = idx; }
+
     public void setGender(String gender){
         this.gender = gender;
     }
 
-    public void setBirth(int age){
+    public void setAge(int age){
         this.age = age;
+    }
+
+    public void setCategory1(String category1) {
+        this.category1 = category1;
+    }
+
+    public void setCategory2(String category2) {
+        this.category2 = category2;
     }
 
     public static synchronized AccountData getInstance(){
