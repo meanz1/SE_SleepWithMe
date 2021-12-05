61dda29eb265426f557e6941ec7f5d455087f8c5
koeunseo2 <koeunseo2@naver.com>
2021-12-05 18:37:51 +0900

    page

--- APP/app/src/main/java/com/example/lullaby/AddAccountActivity.java
+++ APP/app/src/main/java/com/example/lullaby/AddAccountActivity.java
@@ -2,9 +2,15 @@ package com.example.lullaby;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Spinner;
+
+import com.example.lullaby.videos.SelectActivity;
 
 public class AddAccountActivity extends AppCompatActivity {
 
@@ -17,5 +23,13 @@ public class AddAccountActivity extends AppCompatActivity {
                 R.array.birth, android.R.layout.simple_spinner_item);
         brAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         brSpinner.setAdapter(brAdapter);
+
+        Button saveButton = findViewById(R.id.add_save);
+        saveButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
